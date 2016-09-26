fd26d4988c327d4503a25bfbe86ed2bc51b86bbb
sonnyhuddart <sonny.huddart@live.co.uk>
2016-09-26 14:17:11 +0100

    Add files via upload

---
+++ Challenge10.java
@@ -0,0 +1,13 @@
+package challenge.pkg10;
+
+public class Challenge10 {
+
+   
+    public static void main(String[] args) {
+        for(int i = 1; i <= 12; i++){
+            System.out.println(i*10);
+        }
+       
+    }
+    
+}
